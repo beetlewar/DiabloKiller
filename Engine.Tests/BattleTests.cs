31880103c8b77e510dd20f4a6ffd0a9355678007
beetlewar <beetlewar@mail.ru>
2015-12-16 01:11:08 +0300

    1

---
+++ Engine.Tests/BattleTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Engine.Tests
+{
+    [TestFixture]
+    public class BattleTests
+    {
+        [Test]
+        public void Ctor_NullHeroe_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Battle(null));
+        }
+
+        [Test]
+        public void Ctor_NonNullHeroe_StoresHeroe()
+        {
+            var h = MockRepository.GenerateStub<IHeroe>();
+            var b = new Battle(h);
+            Assert.AreSame(h, b.Heroe);
+        }
+
+        [Test]
+        public void Execute_HeroesChanceToWinLessThanMaxChance_CallsRandomizeBasedOnHeroesChance()
+        {
+            var random = MockRepository.GenerateMock<IRandomizer>();
+            random.Expect(r => r.RandomizeBool(0.3f)).Return(false);
+
+            var heroe = MockRepository.GenerateStub<IHeroe>();
+            heroe.Stub(h => h.Power).Return(2);
+
+            var staticVal = MockRepository.GenerateStub<IStaticValues>();
+            staticVal.Stub(s => s.MaxChanceToWin).Return(0.5f);
+            staticVal.Stub(s => s.BaseChanceToWin).Return(0.2f);
+            staticVal.Stub(s => s.PowerEffectToWin).Return(0.05f);
+
+            new Battle(heroe, random, staticVal).Execute();
+
+            random.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void Execute_HeroesChanceToWinGreaterThanMaxChance_CallsRandomizeBasedOnMaxChance()
+        {
+            var random = MockRepository.GenerateMock<IRandomizer>();
+            random.Expect(r => r.RandomizeBool(0.5f)).Return(false);
+
+            var heroe = MockRepository.GenerateStub<IHeroe>();
+            heroe.Stub(h => h.Power).Return(10);
+
+            var staticVal = MockRepository.GenerateStub<IStaticValues>();
+            staticVal.Stub(s => s.MaxChanceToWin).Return(0.5f);
+            staticVal.Stub(s => s.BaseChanceToWin).Return(0.2f);
+            staticVal.Stub(s => s.PowerEffectToWin).Return(0.05f);
+
+            new Battle(heroe, random, staticVal).Execute();
+
+            random.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void Execute_LosesTheBattle_DecreasesHealthBySpecifiedValue()
+        {
+            var heroe = MockRepository.GenerateMock<IHeroe>();
+            heroe.Expect(h => h.DecreaseHealth(20.0f));
+
+            var random = MockRepository.GenerateStub<IRandomizer>();
+            random.Stub(r => r.RandomizeBool(default(float))).IgnoreArguments().Return(false);
+
+            var staticVal = MockRepository.GenerateStub<IStaticValues>();
+            staticVal.Stub(s => s.HealthLostAfterDefeat).Return(20.0f);
+
+            new Battle(heroe, random, staticVal).Execute();
+
+            heroe.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void Execute_WinsTheBattle_DecreasesHealthRelBySpecifiedValue()
+        {
+            var heroe = MockRepository.GenerateMock<IHeroe>();
+            heroe.Expect(h => h.DecreaseHealthRel(0.2f));
+
+            var random = MockRepository.GenerateStub<IRandomizer>();
+            random.Stub(r => r.RandomizeBool(default(float))).IgnoreArguments().Return(true);
+
+            var staticVal = MockRepository.GenerateStub<IStaticValues>();
+            staticVal.Stub(s => s.HealthLostAfterWinRel).Return(0.2f);
+
+            new Battle(heroe, random, staticVal).Execute();
+
+            heroe.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void Execute_WinsTheBattle_IncreasesCoinsBySpecifiedValue()
+        {
+            var heroe = MockRepository.GenerateMock<IHeroe>();
+            heroe.Expect(h => h.AddCoins(3));
+
+            var random = MockRepository.GenerateStub<IRandomizer>();
+            random.Stub(r => r.RandomizeBool(default(float))).IgnoreArguments().Return(true);
+
+            var staticVal = MockRepository.GenerateStub<IStaticValues>();
+            staticVal.Stub(s => s.CoinsGainedAfterWin).Return(3);
+
+            new Battle(heroe, random, staticVal).Execute();
+
+            heroe.VerifyAllExpectations();
+        }
+    }
+}
---
+++ Engine.Tests/HeroeTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Engine.Tests
+{
+    [TestFixture]
+    public class HeroeTests
+    {
+        [Test]
+        public void Ctor_StubStaticValues_InitializesParametersFromStaticValues()
+        {
+            var staticVal = MockRepository.GenerateStub<IStaticValues>();
+            staticVal.Stub(s => s.DefaultHeroeCoins).Return(3);
+            staticVal.Stub(s => s.DefaultHeroeHealth).Return(33.2f);
+            staticVal.Stub(s => s.DefaultHeroeMaxHealth).Return(55.5f);
+            staticVal.Stub(s => s.DefaultHeroePower).Return(129);
+
+            var h = new Heroe(staticVal);
+
+            Assert.AreEqual(3, h.Coins);
+            Assert.AreEqual(33.2f, h.Health);
+            Assert.AreEqual(55, 5f, h.MaxHealth);
+            Assert.AreEqual(129, h.Power);
+        }
+
+        [Test]
+        public void DecreaseHealthRel_SetsExpectedHealth()
+        {
+            var staticVal = MockRepository.GenerateStub<IStaticValues>();
+            staticVal.Stub(s => s.DefaultHeroeHealth).Return(50);
+
+            var h = new Heroe(staticVal);
+            h.DecreaseHealthRel(0.5f);
+
+            Assert.AreEqual(25.0f, h.Health);
+        }
+
+        [Test]
+        public void DecreaseHealth_SetsExpectedHealth()
+        {
+            var staticVal = MockRepository.GenerateStub<IStaticValues>();
+            staticVal.Stub(s => s.DefaultHeroeHealth).Return(77);
+
+            var h = new Heroe(staticVal);
+            h.DecreaseHealth(10);
+
+            Assert.AreEqual(67, h.Health);
+        }
+
+        [Test]
+        public void DecreaseHealth_NotEnoghHealth_SetsHealthToZero()
+        {
+            var staticVal = MockRepository.GenerateStub<IStaticValues>();
+            staticVal.Stub(s => s.DefaultHeroeHealth).Return(30);
+
+            var h = new Heroe(staticVal);
+            h.DecreaseHealth(40);
+
+            Assert.AreEqual(0, h.Health);
+        }
+
+        [Test]
+        public void DecreaseHealth_NotEnoghHealth_RaisesDeathEvent()
+        {
+            var staticVal = MockRepository.GenerateStub<IStaticValues>();
+            staticVal.Stub(s => s.DefaultHeroeHealth).Return(40);
+
+            var h = new Heroe(staticVal);
+
+            h.Died += (s, ea) => Assert.Pass();
+
+            h.DecreaseHealth(40);
+
+            Assert.Fail();
+        }
+
+        [Test]
+        public void AddCoins_SetsExpectedCoinsCount()
+        {
+            var staticVal = MockRepository.GenerateStub<IStaticValues>();
+            staticVal.Stub(s => s.DefaultHeroeCoins).Return(33);
+
+            var h = new Heroe(staticVal);
+            h.AddCoins(22);
+
+            Assert.AreEqual(55, h.Coins);
+        }
+    }
+}
