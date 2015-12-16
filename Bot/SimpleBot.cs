4b09dcbd38a2b1ed823c69964a010cb6fbec2f85
beetlewar <beetlewar@mail.ru>
2015-12-16 16:39:21 +0300

    1

---
+++ Bot/SimpleBot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine;
+
+namespace Bot
+{
+    public class SimpleBot : ACommand
+    {        
+        public SimpleBot(IHero hero, IStaticValues staticValues) :
+            this(hero, null, staticValues)
+        {
+        }
+
+        internal SimpleBot(
+            IHero hero, 
+            IRandomizer randomizer,
+            IStaticValues staticValues) :
+            base(hero, randomizer, staticValues, null)
+        {
+        }
+
+        public override string Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return "бот определяет, что делать";
+        }
+    }
+}
