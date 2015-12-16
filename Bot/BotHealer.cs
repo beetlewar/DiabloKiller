ac181b9cb6d0408f690d3c4299b6af7c8fe82530
beetlewar <beetlewar@mail.ru>
2015-12-16 18:27:52 +0300

    1

---
+++ Bot/BotHealer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine;
+
+namespace Bot
+{
+    public class BotHealer :
+        ABotCommand
+    {    
+        public BotHealer(
+            IHero hero,
+            IStaticValues staticValues) :
+            this(hero, staticValues, new Healer(hero, staticValues))
+        {
+        }
+
+        internal BotHealer(
+            IHero hero,
+            IStaticValues staticValues,
+            ICommand command) :
+            base(hero, staticValues, command)
+        {
+        }
+
+        public override BotCommandAvailability Availability
+        {
+            get 
+            {
+                if (this.Hero.Coins >= this.StaticValues.HealPrice)
+                {
+                    return
+                        this.Hero.Health < this.Hero.Health ?
+                        BotCommandAvailability.Allowed :
+                        BotCommandAvailability.NotRecommended;
+                }
+                else
+                {
+                    return BotCommandAvailability.Forbidden;
+                }
+            }
+        }
+
+        public override int Order
+        {
+            get { return 3; }
+        }
+    }
+}
